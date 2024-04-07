0c0c573928308862ed31701363fd6f4b04b2944a
Kamil Milewski <91847643+K4mi1ewski@users.noreply.github.com>
2024-04-07 22:25:25 +0200

    add lab08

---
+++ lab08/include/Product.h
@@ -0,0 +1,56 @@
+#ifndef PRODUCT_H
+#define PRODUCT_H
+#include <utility>
+#include <iostream>
+using namespace std;
+
+class Product
+{
+    int type;
+    int amount;
+
+    public:
+    /**
+    *@brief Konstruktor dwuargumentowy
+    *@param [in] t Wartosc, za pomoca ktorej inicjalizujemy zmienna 'type'
+    *@param [in] a Wartosc, za pomoca ktorej inicjalizujemy zmienna 'amount'
+    */
+    Product(int t, int a);
+    
+    /**
+    *@brief Konstruktor kopiujacy
+    *@param [in] prod Obiekt typu Product, z ktorego wartosci kopiujemy
+    */
+    Product(const Product& prod);
+    
+    /**
+    *@brief Konstruktor przenoszacy
+    *@param [in] p Obiekt typu Product, z ktorego przenosimy wartosci
+    */
+    Product (Product&& p);
+    
+    /**
+    *@brief Konstruktor bezargumentowy\
+    *Ustawia wartosci zmiennych na 0
+    */
+    Product();
+    
+    /**
+    *@brief Metoda wypisujaca dane 
+    *Metoda wypisuje na ekran wartosci
+    *@return metoda nic nie zwraca
+    */
+    void Print();
+
+    /**
+    *@brief Metoda zmieniajaca ilosc
+    *@param [in] card Nowa ilosc ktora przypisujemy do zmiennej 'amount'
+    *@return metoda nic nie zwraca
+    */
+    void SetCard(int card);
+
+};
+
+
+
+#endif
---
+++ lab08/src/Product.cpp
@@ -0,0 +1,28 @@
+#include "Product.h"
+
+
+
+Product::Product(int t, int a): type(t), amount(a)
+{}
+
+void Product::Print()
+{
+    cout << "typ: " << type << ", ilosc sztuk: " << amount << endl;
+}
+
+void Product::SetCard(int card)
+{
+    amount = card;
+}
+
+
+Product::Product(const Product& prod): type(prod.type), amount(prod.amount)
+{}
+
+Product::Product (Product&& p)
+{
+    type = move(p.type);
+    amount = move(p.amount);
+}
+
+Product::Product(): type(0),amount(0){}
